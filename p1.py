82b98e99a45b043ae684daa339aa4b043ad55f49
SHUBHAM YADAV <93042979+shubhamyadav4665@users.noreply.github.com>
2024-02-11 11:35:37 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,9 @@
+
+std_name=str(input("Enter your name: "))
+En_no=str(input("Enter your EN. No.:"))
+course=str(input("Enter your course:"))
+clg_name=str(input("Enter your college name:"))
+print("Hello ",std_name)
+print("Enrollment Number:",En_no)
+print("course:",course)
+print("college name:",clg_name)
---
+++ p2.py
@@ -0,0 +1,18 @@
+num_1=int(input("Enter Num_1: "))
+num_2=int(input("Enter Num_2:"))
+#---Arithmetic Operator---
+print("{0}+{1}={2}".format(num_1,num_2,num_1+num_2))
+print("{0}-{1}={2}".format(num_1,num_2,num_1-num_2))
+print("{0}*{1}={2}".format(num_1,num_2,num_1*num_2))
+print("{0}/{1}={2}".format(num_1,num_2,num_1/num_2))
+print("{0}%{1}={2}".format(num_1,num_2,num_1%num_2))
+#---Raltional Operator---
+print("{0}={1}is{2}".format(num_1,num_2,num_1==num_2))
+print("{0}not={1}is{2}".format(num_1,num_2,num_1!=num_2))
+print("{0}>{1}is{2}".format(num_1,num_2,num_1>num_2))
+print("{0}<{1}is{2}".format(num_1,num_2,num_1<num_2))
+print("{0}>={1}is{2}".format(num_1,num_2,num_1>=num_2))
+print("{0}<={1}is{2}".format(num_1,num_2,num_1<=num_2))
+#---Logical Operator---
+print("num_1&num_2:",num_1&num_2)
+print("num_1|num_2:",num_1|num_2)
