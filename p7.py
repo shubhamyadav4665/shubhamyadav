82481e763794542df7a388773debc56f60bbf854
SHUBHAM YADAV <93042979+shubhamyadav4665@users.noreply.github.com>
2024-02-28 14:35:02 +0530

    Create p7.py

---
+++ p7.py
@@ -0,0 +1,9 @@
+#swaping of number
+num1=int(input("enter no1 : "))
+num2=int(input("enter no2 : "))
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("after swaping")
+print("no 1=",num1)
+print("no 2=",num2)
